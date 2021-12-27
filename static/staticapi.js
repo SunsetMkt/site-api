19b5adc23c3a2fc96670ec8e6b47c172d219b1eb
Mikoto <26019675+lwd-temp@users.noreply.github.com>
2021-12-27 22:29:30 +0800

    Update staticapi.js

--- static/staticapi.js
+++ static/staticapi.js
@@ -1,5 +1,22 @@
-console.log("Loaded JavaScript Snippet from API.");
+console.log("Loaded JavaScript snippet from API.");
 
-window.onload = function () {
+function steamStoreMsg() {
     document.body.innerHTML = "<p>请关注Steam商店在中国大陆的访问受阻问题。<a href='https://jekyll.lwd-temp.top/article/steam-banned/'>详细信息</a></p>" + document.body.innerHTML;
 };
+
+function ready(fn) {
+    if (document.addEventListener) {//标准浏览器
+        document.addEventListener('DOMContentLoaded', function () {
+            //注销时间，避免重复触发
+            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+            steamStoreMsg();//运行函数
+        }, false);
+    } else if (document.attachEvent) {//IE浏览器
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState == 'complete') {
+                document.detachEvent('onreadystatechange', arguments.callee);
+                steamStoreMsg();//函数运行
+            }
+        });
+    }
+}
