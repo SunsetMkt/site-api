c94295fb6b95628548f2cbec6ece672e21a205c5
Mikoto <26019675+lwd-temp@users.noreply.github.com>
2021-12-27 21:14:03 +0800

    Create staticapi.js

---
+++ static/staticapi.js
@@ -0,0 +1,5 @@
+console.log("Loaded JavaScript Snippet from API.");
+
+window.onload = function () {
+    document.body.innerHTML = "<p>请关注Steam商店在中国大陆的访问受阻问题。<a href='https://jekyll.lwd-temp.top/article/steam-banned/'>详细信息</a></p>" + document.body.innerHTML;
+};
