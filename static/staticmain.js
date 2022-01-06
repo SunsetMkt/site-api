9a3f452706a2378ef1a74db5b5ac96ee5d0ddc4e
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-01-06 22:42:06 +0800

    文案修改

--- static/staticmain.js
+++ static/staticmain.js
@@ -2,7 +2,7 @@ console.log("Loaded JavaScript Snippet from API.");
 
 function pushedMsg() {
 	var msg = document.createElement("p");
-	msg.innerHTML = "最近事可不少，若有闲心便点进去看看吧。<a href='https://jekyll.lwd-temp.top/article/script-of-a-dark-fairytale/'>详细信息</a>";
+	msg.innerHTML = "“千言万语，此时也只能汇作相伴”，若有闲心便点进去看看吧。<a href='https://jekyll.lwd-temp.top/article/script-of-a-dark-fairytale/'>详细信息</a>";
 	document.getElementsByTagName("body")[0].insertBefore(msg, document.body.firstElementChild);
 };
 
