418ed4f4f58717b62c52c8536e57550e3bcfaa05
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-02-28 17:57:33 +0800

    bug fix

--- api/kizunaai_demo.php
+++ api/kizunaai_demo.php
@@ -4,7 +4,7 @@
      * 绊爱日历项目云端API
      * 参数：id = 0630 客户端ID
      *     date = MM-DD格式，需要与服务器时间相同 例如01-01 02-22 12-31
-     * API返回：用于直接获取OSS（对象存储服务，可以理解为网盘）上文件的URL
+     * API返回值：用于直接获取OSS（对象存储服务，可以理解为网盘）上文件的URL，纯文本
     */
     
     /*
@@ -43,13 +43,13 @@
                 //$dir="./videos/KizunaAI";
                 //$video_floder=scandir($dir);//获取视频主目录下所有文件夹
                 //遍历数组
-                foreach($file_struct as $i)
-                {
+                //foreach($file_struct as $i)
+                //{
                     //根据客户端传来的日期确定相应名称的文件夹
-                    if($client_date == $i)
-                    {
+                    //if($client_date == $i)
+                    //{
                         // $video=scandir($dir."/".$i);//获取视频分目录下所有文件
-                        $videos=array_values($file_struct[$i]);
+                        $videos=array_values($file_struct[$i]);//直接根据日期获取视频列表
                         // $new_video=array_slice($video,2);//截取数组 // 不再需要，我们的json里不会出现'.'和'..'
                         if(count($videos) > 1)//count($new_video)获取数组长度
                         {
@@ -64,8 +64,8 @@
                             $oss_url=$oss_base_url_a.$abs_dir.$oss_base_url_b;
                             echo $oss_url;
                         }
-                    }
-                }
+                    //}
+                //}
             } 
             else
             {
