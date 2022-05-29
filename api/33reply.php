e5b71c52a58fe721e1662f8d5c212f60c153c0de
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-05-29 08:46:48 +0800

    Update 33reply.php

--- api/33reply.php
+++ api/33reply.php
@@ -1,11 +1,22 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-    	header('Access-Control-Allow-Headers: x-requested-with');
+header("Access-Control-Allow-Origin: *");
 
-	$ch = curl_init('https://'.'api.bilibili.com/x/v2/reply/main'.'?jsonp=jsonp&next=0&type=17&oid=662016827293958168&mode=2&plat=1');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//处理next和oid值
+if (isset($_GET['next'])) {
+    $next = $_GET['next'];
+} else {
+    $next = "0";
+}
 
-	$result = curl_exec($ch);
+if (isset($_GET['oid'])) {
+    $oid = $_GET['oid'];
+} else {
+    $oid = "662016827293958168";
+}
 
-	echo $result;
+$ch = curl_init('https://api.bilibili.com/x/v2/reply/main' . '?jsonp=jsonp&next=' . $next . '&type=17&oid=' . $oid . '&mode=2&plat=1');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+
+echo $result;
