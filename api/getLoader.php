ed0272cb8e70975ea3040d4911431d3a10c987a5
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-07-04 22:25:48 +0800

    API for apijs

---
+++ api/getLoader.php
@@ -0,0 +1,19 @@
+<?php
+// CORS
+header('Access-Control-Allow-Origin: *');
+
+// Open file
+$file = fopen(__DIR__ . '/../static/staticapi.js', "r");
+// Read file
+$content = fread($file, filesize(__DIR__ . '/../static/staticapi.js'));
+// Close file
+fclose($file);
+
+// mime type
+header('Content-Type: text/javascript');
+// no cache
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+// output
+echo $content;
+?>
