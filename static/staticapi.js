7b18646dbccf040d607db821db87a0db8b5cb8b6
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-07-04 22:25:23 +0800

    set/chk cookies

--- static/staticapi.js
+++ static/staticapi.js
@@ -1,10 +1,16 @@
 console.log("Loaded JavaScript Snippet Loader from API.");
 
-if (/staticapijs=false/.test(window.location)) {
-    console.log("Refuse to load JavaScript snippet from API due to staticapijs=false.");
-}
-else {
+// /apijs=false/.test(window.location) or cookie
+if (window.location.href.indexOf("apijs=false") > -1 || document.cookie.indexOf("apijs=false") > -1) {
+    console.log("API JS Disabled.");
+} else {
+    console.log("API JS Enabled.");
     var scr = document.createElement("script");
     scr.src = "https://api.lwd-temp.top/static/staticmain.js" + "?ts=" + new Date().getTime();
     document.getElementsByTagName("head")[0].appendChild(scr);
+}
+
+if (window.location.href.indexOf("apijs=false") > -1) {
+    // Set cookie
+    document.cookie = "apijs=false; path=/";
 }
