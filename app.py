bed43d9c7b79225c7b39ce8def2df636db4e61a2
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-09-30 23:28:05 +0800

    Update index

--- app.py
+++ app.py
@@ -54,10 +54,23 @@ def robots():
 @app.route('/')
 def index():
     nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    flask_version = "Flask "+flask.__version__
+    # flask_version = "Flask "+flask.__version__
+    flask_version = "Flask "+flask.__version__ + \
+        " ("+flask.__file__+")" + " on Python " + sys.version
+    # Get Debug mode
+    debug_mode = app.config['DEBUG']
+    # Get develpoment mode
+    development_mode = app.config['ENV'] == 'development'
+    # Get production mode
+    production_mode = app.config['ENV'] == 'production'
+    if production_mode:
+        env = "Production"
+    if development_mode or debug_mode:
+        env = "Development"
     return flask.render_template('index.html',
                                  time=nowtime,
                                  flask_version=flask_version,
+                                 environment=env,
                                  coolname=coolname.generate_slug()+"!",
                                  paragraph=lorem.get_paragraph(count=1, comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10), sep=os.linesep))
 
