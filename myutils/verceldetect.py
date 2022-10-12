8156177ee700b6afb68e7428a08cfbfa7d224269
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-10-13 00:23:33 +0800

    Detect Vercel environment

--- myutils/verceldetect.py
+++ myutils/verceldetect.py
@@ -4,10 +4,18 @@ import os
 
 
 def isVercel():
-    # Get all environment variables
-    env = os.environ
-    # Check if keyword "VERCEL" is in the environment variables
-    for key in env:
-        if "VERCEL" in key:
+    # Check if linux
+    if os.name == 'posix':
+        # Get all environment variables
+        env = os.environ
+        # Check if keyword "VERCEL" is in the environment variables
+        for key in env:
+            if "VERCEL" in key:
+                return True
+        # Check if "/var/task/vercel.json" exists
+        if os.path.exists("/var/task/vercel.json"):
             return True
+    else:
+        return False
+
     return False
