aa92fbfe7793a9cf2f9f54bc50f7537f7b671e48
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-10-17 11:32:01 +0800

    load swagger res from cdn

--- app.py
+++ app.py
@@ -8,14 +8,20 @@ import http
 import os
 import traceback
 
+import flasgger
 import flask
 import flask_cors
-import flasgger
 
 import myutils
 import myutils.api_v1
 
 app = flask.Flask(__name__)
+
+swagger_config = flasgger.Swagger.DEFAULT_CONFIG
+swagger_config['swagger_ui_bundle_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js'
+swagger_config['swagger_ui_standalone_preset_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js'
+swagger_config['jquery_js'] = '//unpkg.com/jquery@2.2.4/dist/jquery.min.js'
+swagger_config['swagger_ui_css'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui.css'
 swagger_template = {
     "info": {
         "title": "site-api",
