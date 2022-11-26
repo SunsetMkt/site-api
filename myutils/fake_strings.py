666a7ba4e9b7d147730c30837610606d788858c5
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-11-26 21:25:37 +0800

    better

--- myutils/fake_strings.py
+++ myutils/fake_strings.py
@@ -1,10 +1,23 @@
+import random
+
 import faker
 
 fake = faker.Faker(locale='zh_CN')
 
 
 def fake_article():
-    return fake.text(max_nb_chars=500)
+    sentenses = fake.texts(nb_texts=10, max_nb_chars=500)
+    full_text = ''
+    for s in sentenses:
+        sf = ''
+        for a in s:
+            dot = random.choice(['。', '？', '！', '，', '，', '，', '，', '，',
+                                '，', '，', '，', '，', '，', '。', '。', '。', '。', '。', '。'])
+            if a == '.':
+                a = dot
+            sf = sf + a
+        full_text = full_text + sf
+    return full_text.replace('\n', '')
 
 
 if __name__ == '__main__':
