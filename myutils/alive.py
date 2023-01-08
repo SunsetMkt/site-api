dd25515c8383bb80f0e83f520fe1157d9300e5c8
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-01-08 16:18:51 +0800

    bug fix

--- myutils/alive.py
+++ myutils/alive.py
@@ -12,11 +12,18 @@ red_alert_days = 28
 
 
 def get_github_last_activity_time(name):
-    url = 'https://api.github.com/users/' + name
+    # https://stackoverflow.com/a/37554614/20675299
+    url = 'https://api.github.com/users/' + name + '/events'
     r = requests.get(url)
     r.raise_for_status()
-    updatedat = r.json()['updated_at']
-    return datetime.datetime.strptime(updatedat, '%Y-%m-%dT%H:%M:%SZ')
+    # get latest event time
+    try:
+        latest = r.json()[0]['created_at']
+    except IndexError:
+        # 时间表中只包含过去 90 天内创建的事件。 超过 90 天的活动将不包括在内（即使时间表中的活动总数不到 300 个）。
+        latest = '1970-01-01T00:00:00Z'
+
+    return datetime.datetime.strptime(latest, '%Y-%m-%dT%H:%M:%SZ')
 
 
 def get_days_since(time):
@@ -29,6 +36,8 @@ def get_alive_info(name):
     except:
         return 'error', 'error', 'error'
     days = get_days_since(time)
+    if days > 90:
+        return 'timeout', days, time
     if days > red_alert_days:
         return 'red', days, time
     elif days > yellow_alert_days:
@@ -49,6 +58,11 @@ def render():
         title = '无法获取活跃信息'
         subtitle = f'无法获取 {name} 的活跃信息'
         message = ''
+    elif color == 'timeout':
+        color = 'red'
+        title = '红色警报'
+        subtitle = f'{name} 在 GitHub 上无活动超过90天'
+        message = f'最后活跃时间未知，因为 GitHub API 仅提供最近90天的活动。这是一个演示目的的页面，警报无实际意义。'
     elif color == 'green':
         title = '一切正常'
         subtitle = f'{name} 在 {time_to_str(time)} 在 GitHub 上有活动，至今 {days} 天'
