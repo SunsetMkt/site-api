a3a82d341ea56cdcbb71ffc70d1b9a5e9a8a80b5
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-02-24 12:59:06 +0800

    subs

--- myutils/clash.py
+++ myutils/clash.py
@@ -113,7 +113,7 @@ def render():
     subscribe_source_name, subscribe_source_url, subscribe_url, base64_url = subscribe()
 
     # URL Encoded
-    subscribe_encoded_url = urllib.parse.quote(subscribe_url)
+    subscribe_encoded_url = urllib.parse.quote(subscribe_url, safe='')
 
     return flask.render_template('clash.html', cfw=cfw(), cfw_portable=cfw_portable(), cfa=cfa(), subscribe_source_name=subscribe_source_name, subscribe_source_url=subscribe_source_url, subscribe_url=subscribe_url, base64_url=base64_url, subscribe_encoded_url=subscribe_encoded_url)
 
@@ -121,22 +121,74 @@ def render():
 def config():
     # Get yaml and return
     # https://raw.githubusercontent.com/paimonhub/Paimonnode/main/clash.yaml
-    config_urls = [
+    sub_urls = [
         "https://raw.githubusercontent.com/paimonhub/Paimonnode/main/clash.yaml",
-        "https://sub.pmsub.me/clash.yaml",
-        "https://sub.xeton.dev/sub?target=clash&new_name=true&url=%7Chttps%3A%2F%2Fraw.githubusercontent.com%2FPawdroid%2FFree-servers%2Fmain%2Fsub%7Chttps%3A%2F%2Fraw.githubusercontent.com%2Fopenrunner%2Fclash-freenode%2Fmain%2Fclash.yaml%7Chttps%3A%2F%2Fraw.githubusercontent.com%2Flearnhard-cn%2Ffree_proxy_ss%2Fmain%2Fconfig.yaml&insert=false&append_type=true&emoji=true&list=false&tfo=false&scv=false&fdn=true&sort=true",
-        "https://sub.xeton.dev/sub?target=clash&new_name=true&url=https%3A%2F%2Fraw.githubusercontent.com%2FPawdroid%2FFree-servers%2Fmain%2Fsub&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini",
+        "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
         "https://raw.githubusercontent.com/openrunner/clash-freenode/main/clash.yaml",
         "https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/config.yaml"
     ]
 
-    for url in config_urls:
-        config = requests.get(url)
-        if config.status_code != 200:
+    api_urls = [
+        "https://sub.xeton.dev/sub",
+        "https://api.wcc.best/sub",
+        "https://api.dler.io/sub"
+    ]
+
+    # Get an available API url
+    api_url = False
+    for url in api_urls:
+        try:
+            api = requests.get(url)
+        except:
             continue
-        else:
-            config.raise_for_status()
-            config = config.text
+
+        if api.status_code == 400:
+            api_url = url
             break
+
+    if api_url:
+        # API is available
+
+        # Get sub list
+        subs = []
+        for url in sub_urls:
+            try:
+                config = requests.get(url)
+            except:
+                continue
+
+            if config.status_code == 200:
+                subs.append(url)
+
+        # API args
+        # target=clash&new_name=true&url=
+        # &insert=false&append_type=true&emoji=true&list=false&tfo=false&scv=false&fdn=true&sort=true
+        api_call = api_url + "?"
+        url_cmb = ""
+        for sub in subs:
+            url_cmb = url_cmb + "|"
+            url_cmb = url_cmb + sub
+        api_args = "target=clash&new_name=true&url=" + url_cmb + \
+            "&insert=false&append_type=true&emoji=true&list=false&tfo=false&scv=false&fdn=true&sort=true"
+        api_call = api_call + urllib.parse.quote(api_args, safe='')
+        api_req = requests.get(api_call)
+        api_req.raise_for_status()
+        config = api_req.text
+
+    else:
+        # No API available
+        for url in sub_urls:
+
+            try:
+                config = requests.get(url)
+            except:
+                continue
+
+            if config.status_code != 200:
+                continue
+            else:
+                # config.raise_for_status()
+                config = config.text
+                break
 
     return config
