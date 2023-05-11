24ea92a82f7f0635a3cd57e5d681be1f068f7294
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-05-11 17:59:09 +0800

    Update

--- myutils/clash.py
+++ myutils/clash.py
@@ -195,12 +195,13 @@ def config(base64=False, append_url=None):
         # "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml", # 效果不佳。
         # "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/2/config.yaml", # 效果不佳。
         # "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml", # 效果不佳。
-        "https://sub.sharecentre.online/sub",  # 信任
-        "https://get.ooop.ink",  # 能用
+        "https://sub.sharecentre.online/sub",  # 信任 # https://t.me/ShareCentrePro
+        # "https://get.ooop.ink",  # 能用
+        "https://get.noaries.app",  # https://t.me/aries_init
         # "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/clash.yml" # 长期不更新
         # "https://github.com/yu-steven/openit" # 纪念
-        "https://sub.yxjnode.com/sub",  # 来自sharecentre
-        "https://sub.pmsub.me/clash.yaml",  # 效果不佳，置后
+        "https://sub.yxjnode.com/sub",  # 来自sharecentre # https://t.me/ShareCentrePro
+        "https://sub.pmsub.me/clash.yaml",  # 效果不佳，置后 # https://t.me/nodpai
     ]
 
     api_urls = [
