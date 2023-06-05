726c68229572cdb3718567352e96f7839e2a4404
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-06-05 20:58:15 +0800

    update

--- myutils/clash.py
+++ myutils/clash.py
@@ -197,7 +197,7 @@ def config(base64=False, append_url=None):
         # "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml", # 效果不佳。
         "https://sub.sharecentre.online/sub",  # 信任 # https://t.me/ShareCentrePro
         # "https://get.ooop.ink",  # 能用
-        "https://get.noaries.app",  # https://t.me/aries_init
+        "https://woc.ooop.ink",  # https://t.me/aries_init
         # "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/clash.yml" # 长期不更新
         # "https://github.com/yu-steven/openit" # 纪念
         "https://sub.yxjnode.com/sub",  # 来自sharecentre # https://t.me/ShareCentrePro
