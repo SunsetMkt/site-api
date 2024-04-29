ad7e7ba411eaafd796e84a38c542969afb91aad0
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-04-29 22:34:38 +0800

    ?

--- myutils/clash.py
+++ myutils/clash.py
@@ -222,7 +222,7 @@ def config(base64=False, append_url=None):
         # "https://woc.ooop.ink",  # https://t.me/aries_init
         # "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/clash.yml" # 长期不更新
         # "https://github.com/yu-steven/openit" # 纪念
-        "https://sub.pmsub.me/clash.yaml",  # https://t.me/nodpai
+        # "https://sub.pmsub.me/clash.yaml",  # https://t.me/nodpai
         "https://gitlab.com/free9999/ipupdate/-/raw/master/clash.meta/config.yaml",
         "https://gitlab.com/free9999/ipupdate/-/raw/master/clash.meta/2/config.yaml",
         "https://gitlab.com/free9999/ipupdate/-/raw/master/clash.meta/3/config.yaml",
